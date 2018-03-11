70867f67f8b1c7003e1098b9c02ff1ad673d73fb
Joe Reckley <jdreckley@gmail.com>
2018-03-11 21:30:08 +0800

    updates loading script to resolve after images are loaded, adds carousel images

--- src/images.js
+++ src/images.js
@@ -3,7 +3,9 @@ const URLs = [
   "assets/carousel-2.jpg",
   "assets/carousel-3.jpg",
   "assets/carousel-4.jpg",
-  "assets/carousel-5.jpg"
+  "assets/carousel-5.jpg",
+  "assets/carousel-6.jpg",
+  "assets/carousel-7.jpg",
 ];
 
 export const LOADED_IMAGES = 'LOADED_IMAGES';
@@ -13,7 +15,7 @@ export const loadImages = () => {
     const promises = URLs.map(url => {
       return new Promise((resolve, reject) => {
         const image = new Image;
-        image.onload = resolve;
+        image.onload = () => resolve();
         image.src = url;
       });
     });
